8e575c97fc404b5db1922beb0d242a616bf8e83b
w1nte <github@winterspacher.at>
2021-07-27 17:36:45 +0200

    add utils setup file

---
+++ utils/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='mwinters-thesis-utils',
+    version='1.0',
+    packages=[''],
+    url='',
+    install_requires=[
+        'pandas~=1.2.4',
+        'matplotlib~=3.4.1'
+    ],
+    license='',
+    author='winte',
+    author_email='',
+    description='',
+    entry_points={
+        'console_scripts': [
+            'cbench=cbench:main',
+            'cbench2csv=cbench2csv:main',
+            'cbenchgraph=cbenchgraph:main',
+            'gantt=gantt:main'
+        ]
+    }
+)
