e6e2d62caa74da116676e7e629a4704efa090270
w1nte <github@winterspacher.at>
2021-08-16 14:17:40 +0200

    fix gantt.py + add heuristic + improve clingo-dl encoding

--- utils/gantt.py
+++ utils/gantt.py
@@ -4,11 +4,7 @@ import re
 import random
 import argparse
 import sys
-
-
-def find_predicates(inp: str, name: str, arity=1):
-    reg = re.compile('{}\({}\)'.format(name, ','.join(['([^,)]+)'] * arity)), re.IGNORECASE | re.MULTILINE)
-    return re.findall(reg, inp)
+from math import ceil
 
 
 def main():
@@ -21,8 +17,7 @@ Custom gantt chart generator. The ASP lp file must contain:
     #show release/3.
     #show first/2.
     #show next/3.
-    #show span/2.
-Example usage: clingo any.lp --quiet=1 | python gantt.py -o gantt.png
+Example usage: clingo any.lp --quiet=1 | python gantt_before_relation.py -o gantt.png
     ''')
 
     parser.add_argument('--output', '-o', help='output gantt graph image', type=str, default=None)
@@ -36,59 +31,89 @@ Example usage: clingo any.lp --quiet=1 | python gantt.py -o gantt.png
         print(line, end='')
         inp += line
 
+    try:
+        inp = inp.split('Answer')[-1]
+    except IndexError:
+        pass
+
     machines = [int(m[1:]) for m in find_predicates(inp, 'machine', 1)]
     jobs = [int(j[1:]) for j in find_predicates(inp, 'job', 1)]
     releases = {(int(m[1:]), int(j[1:])): int(t) for j, m, t in find_predicates(inp, 'release', 3)}
+    setups = {(int(j1[1:]), int(j2[1:]), int(m[1:])): int(t) for j1, j2, m, t in find_predicates(inp, 'setup', 4)}
     durations = {(int(m[1:]), int(j[1:])): int(t) for j, m, t in find_predicates(inp, 'duration', 3)}
     firsts = {int(m[1:]): int(j[1:]) for j, m in find_predicates(inp, 'first', 2)}
     nexts = {int(j1[1:]): int(j2[1:]) for j1, j2, m in find_predicates(inp, 'next', 3)}
-    setups = {(int(j1[1:]), int(j2[1:]), int(m[1:])): int(t) for j1, j2, m, t in find_predicates(inp, 'setup', 4)}
-    spans = {int(m[1:]): int(t) for m, t in find_predicates(inp, 'span', 2)}
-    max_time = max(spans.values())
 
+    start_positions = calculate_start_positions(machines, firsts, releases, nexts, durations, setups)
+
+    draw_gantt(start_positions, jobs, machines, releases, setups, durations)
+
+    if args.output:
+        plt.savefig(args.output, dpi=300)
+    else:
+        plt.show()
+
+
+def find_predicates(inp: str, name: str, arity=1):
+    reg = re.compile('{}\({}\)'.format(name, ','.join(['([^,)]+)'] * arity)), re.IGNORECASE | re.MULTILINE)
+    return re.findall(reg, inp)
+
+def calculate_start_positions(machines, firsts, releases, nexts, durations, setups):
+    # returns a tuple (start_time, setup_time, machine) for each job.
+    jobs = {}
+    for m in machines:
+        job = firsts.get(m)
+        job_before = None
+        t = releases.get((m, job), 0)
+
+        while job:  # else no further job exists on this machine.
+            job_duration = durations[(m, job)]
+            job_release = releases[(m, job)]
+            job_setup = setups.get((job_before, job, m), 0)
+
+            t = max(job_release, t)
+
+            jobs[job] = (t, job_setup, m)
+
+            t += job_setup + job_duration
+            job_before = job
+            job = nexts.get(job)
+    return jobs
+
+def draw_gantt(start_positions, jobs, machines, releases, setups, durations):
     number_of_machines = len(machines)
-
+    fig, ax = plt.subplots()
+    max_t = 0
     bar_height = 100 / (number_of_machines + 1)
     bar_margin = 100 / (number_of_machines + 4)
     
-    fig, ax = plt.subplots()
-    ax.set_xlim(0, max_time)
+    for job in jobs:
+        t, job_setup, job_machine = start_positions[job]
+        job_duration = durations[(job_machine, job)]
+        job_release = releases[(job_machine, job)]
+
+        x, w = (t + job_setup, job_duration)
+        h = bar_height - bar_margin
+        y = bar_height * job_machine - h / 2
+
+        ax.broken_barh([(t, job_setup)], (y, h), facecolors=(0.9, 0.9, 0.9),
+                        edgecolors=(0.8, 0.8, 0.8))
+        ax.broken_barh([(x, w)], (y, h), facecolors=(random.random(), 0.8, random.random()),
+                        edgecolors=(0.4, 0.4, 0.4))
+        ax.text(x + w / 2, y + h / 2, '{}'.format(job), horizontalalignment='center',
+                verticalalignment='center', fontsize=7)
+        ax.text(job_release, y - 5, str(job), horizontalalignment='center', verticalalignment='center', fontsize=6)
+        ax.plot([job_release, job_release], [y, y + h], linewidth=2, color=(0, 0, 0))
+
+        max_t = max(max_t, x + w)
+
+    ax.set_xlim(0, ceil(max_t * 1.1))
     ax.set_ylim(1, 100)
     ax.set_xlabel('time')
     ax.set_ylabel('machine')
     ax.set_yticks([bar_height * m for m in range(1, number_of_machines + 1)])
     ax.set_yticklabels([str(m) for m in range(1, number_of_machines + 1)])
     ax.grid(True)
-
-    for m in machines:
-        job = firsts.get(m)
-        t = releases.get((m, job), 0)
-
-        while job:  # else no further job exists on this machine.
-            job_next = nexts.get(job)
-            job_duration = durations[(m, job)]
-            job_release = releases[(m, job)]
-
-            t = max(t, job_release)
-
-            x, w = (t, job_duration)
-            h = bar_height - bar_margin
-            y = bar_height * m - h / 2
-
-            ax.broken_barh([(x, w)], (y, h), facecolors=(random.random(), 0.8, random.random()),
-                           edgecolors=(0.4, 0.4, 0.4))
-            ax.text(x + w / 2, y + h / 2, '{}'.format(job), horizontalalignment='center',
-                    verticalalignment='center', fontsize=7)
-            ax.text(job_release, y - 5, str(job), horizontalalignment='center', verticalalignment='center', fontsize=6)
-            ax.plot([job_release, job_release], [y, y + h], linewidth=2, color=(0, 0, 0))
-
-            t += job_duration + setups.get((job, job_next, m), 500)
-            job = job_next
-
-    if args.output:
-        plt.savefig(args.output, dpi=300)
-    else:
-        plt.show()
 
 
 if __name__ == '__main__':
