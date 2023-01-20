514f485de5bdd086c3010bee74ee2104eae4b195
winte <hello@winte.xyz>
2023-01-20 10:59:03 +0100

    rename clingcon encodings, experiments

--- cluster-experiments/01_clingcon-with-before-relation.lp/01_clingcon-with-before-relation.sh
+++ cluster-experiments/01_clingcon-with-before-relation.lp/01_clingcon-with-before-relation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SOLVER="/home/mwinters/miniconda3/bin/clingcon"
-ENCODING="/home/mwinters/cluster-experiments/01_02_clingcon.lp/02_clingcon.lp"
+ENCODING="/home/mwinters/cluster-experiments/01_clingcon-with-before-relation.lp/01_clingcon-with-before-relation.lp"
 INSTANCE=$1
 TIME_LIMIT=$2
 
